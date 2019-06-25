494452cfe730e63c8d95cf733947ee8af6504215
Daniil Fedotov <hairyhum@gmail.com>
2019-06-25 17:02:12 -0400

    Require erlang 22 for mnevis to work correctly

--- include/rabbit.hrl
+++ include/rabbit.hrl
@@ -210,8 +210,8 @@
 
 -define(COPYRIGHT_MESSAGE, "Copyright (C) 2007-2019 Pivotal Software, Inc.").
 -define(INFORMATION_MESSAGE, "Licensed under the MPL.  See https://www.rabbitmq.com/").
--define(OTP_MINIMUM, "21.3").
--define(ERTS_MINIMUM, "10.3").
+-define(OTP_MINIMUM, "22.0").
+-define(ERTS_MINIMUM, "10.4").
 
 %% EMPTY_FRAME_SIZE, 8 = 1 + 2 + 4 + 1
 %%  - 1 byte of frame type
